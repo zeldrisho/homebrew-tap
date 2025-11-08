93e94c63d5f07da2c9bfdb14db0b0e3008c71e97
Zeldris <80939130+zeldrisho@users.noreply.github.com>
2025-11-08 19:32:56 +0700

    initial commit

---
+++ Casks/kdeconnect.rb
@@ -0,0 +1,38 @@
+cask "kdeconnect" do
+  arch arm: "arm64", intel: "x86_64"
+
+  # TODO: eventually also handle release versions (e.g. release-24.08 etc.)?
+  base_url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}"
+
+  version "5331"
+  sha256 arm:   "1383d27f8a4095f1405cb149417386752c8afd0b3e2f25b04297f278e5ecf3b0",
+         intel: "7f7c1067ae9d5e052d264399b743093fce25e3373213848146eaf284f2480117"
+
+  url "#{base_url}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
+  name "KDE Connect"
+  desc "Enabling communication between all your devices"
+  homepage "https://kdeconnect.kde.org/"
+
+  livecheck do
+    url base_url
+    regex(/href=.*?kdeconnect-kde-master-(\d+(?:)+)-macos-clang-#{arch}\.dmg/i)
+  end
+
+  depends_on macos: ">= :monterey"
+
+  app "KDE Connect.app"
+  binary "#{appdir}/KDE Connect.app/Contents/MacOS/kdeconnect-cli",
+         target: "kdeconnect"
+
+  uninstall quit: "org.kde.kdeconnect"
+
+  zap trash: [
+    "~/Library/Application Support/kpeoplevcard",
+    "~/Library/Caches/kdeconnect.sms",
+    "~/Library/Preferences/kdeconnect",
+    "~/Library/Preferences/kdeconnect_runcommand",
+    "~/Library/Preferences/kdeconnect_sendnotifications",
+    "~/Library/Preferences/kdeconnect_share",
+    "~/Library/Preferences/org.kde.kdeconnect.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,25 @@
+[![autoupdate](https://github.com/zeldrisho/homebrew-tap/actions/workflows/autoupdate.yml/badge.svg)](https://github.com/zeldrisho/homebrew-tap/actions/workflows/autoupdate.yml)
+[![casks](https://img.shields.io/github/directory-file-count/zeldrisho/homebrew-tap/Casks?type=file&extension=rb&style=flat&label=casks)](https://github.com/zeldrisho/homebrew-tap/tree/main/Casks)
+[![repo size](https://img.shields.io/github/repo-size/zeldrisho/homebrew-tap.svg?style=flat-square)](https://github.com/zeldrisho/homebrew-tap)
+
+My personal [Homebrew tap](https://docs.brew.sh/Taps) for macOS applications missing from [official taps](https://github.com/Homebrew/homebrew-cask), or alternate versions thereof
+
+## Usage
+
+[Homebrew](https://brew.sh) is the missing package manager for macOS
+
+Add this tap:
+```bash
+brew tap zeldrisho/tap
+```
+
+Install a cask from this tap:
+```bash
+brew install --cask zeldrisho/tap/<cask>
+```
+
+## Casks
+
+|Name|Description|
+|----|-----------|
+|[kdeconnect](https://github.com/zeldrisho/homebrew-tap/blob/main/Casks/kdeconnect.rb)|Enabling communication between all your devices|
