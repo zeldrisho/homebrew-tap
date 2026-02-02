8d10f1fabaf44bbe49462a318ef8988288610fb6
Zeldris <0trims.vehicle@icloud.com>
2026-02-02 13:15:25 +0000

    pass-cli 1.4.1 (new formula)

---
+++ Formula/pass-cli.rb
@@ -0,0 +1,45 @@
+class PassCli < Formula
+  desc "Proton Pass CLI - Command-line interface for Proton Pass"
+  homepage "https://proton.me/pass"
+  version "1.4.1"
+  license "GPL-3.0"
+
+  on_macos do
+    on_intel do
+      url "https://proton.me/download/pass-cli/1.4.1/pass-cli-macos-x86_64"
+      sha256 "ceffa547d14af8ea5acf0963f11ef0d60ee0bd37fd7ed34a4c70ef86d82ad035"
+
+      def install
+        bin.install "pass-cli-macos-x86_64" => "pass-cli"
+      end
+    end
+
+    on_arm do
+      url "https://proton.me/download/pass-cli/1.4.1/pass-cli-macos-aarch64"
+      sha256 "7019050f490d8289c045eca39a6abf3fd480f6bcc3fa7807831241b4ec13d7f1"
+
+      def install
+        bin.install "pass-cli-macos-aarch64" => "pass-cli"
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      The Proton Pass CLI has been installed as 'pass-cli'.
+
+      To get started:
+        pass-cli login
+
+      For help:
+        pass-cli --help
+
+      Documentation: https://protonpass.github.io/pass-cli/
+    EOS
+  end
+
+  test do
+    output = shell_output("#{bin}/pass-cli --version")
+    assert_match(/Proton Pass CLI #{version}/, output)
+  end
+end
